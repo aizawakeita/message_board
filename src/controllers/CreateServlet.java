6deb38437b31c7007f89c02b68f8084158b71d57
keita.aizawa <81903@LAPTOP-A3KIQ86L>
2021-10-19 17:57:22 +0900

    フラッシュメッセージを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -25,15 +25,15 @@ public class CreateServlet extends HttpServlet {
      */
     public CreateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String _token = request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())) {
+        if (_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
             em.getTransaction().begin();
 
@@ -49,18 +49,14 @@ public class CreateServlet extends HttpServlet {
             m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
-            em.persist(m);
-            em.getTransaction().commit();
             // データベースに保存
             em.persist(m);
             em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");       // ここを追記
-            em.close();
+            request.getSession().setAttribute("flush", "登録が完了しました。"); // ここを追記
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
         }
     }
-
 
 }
